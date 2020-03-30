f2c7a2dbf6829b597938cdccb72c527b04c9ec02
Gonçalo Almeida <32540757+xaleza@users.noreply.github.com>
2020-03-30 17:22:08 +0100

    Update ObservationsHashMap.java

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/ObservationsHashMap.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/ObservationsHashMap.java
@@ -2,6 +2,7 @@ package pt.tecnico.sauron.silo.domain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import pt.tecnico.sauron.silo.grpc.Silo.Observation;
 
@@ -9,17 +10,11 @@ public class ObservationsHashMap {
 
     private Map<String, ArrayList<Observation>> observationsHashMap = new HashMap<String, ArrayList<Observation>>();
 
-    public ArrayList<Observation> getObservations(String identifier) {
-        return observationsHashMap.get(identifier);
-    }
-
     public void inputObservation(String identifier , Observation observation) {
-        
-        ArrayList<Observation> observations = getObservations(identifier);
-        observations.add(observation);
-
-        //Replaces current list with the old list plus the new observation
-        observationsHashMap.put(identifier, observations);
-
+        observationsHashMap.get(identifier).add(observation);
     }
+
+    public List<Observation> getObservations(String identifier) {
+        return observationsHashMap.get(identifier);
+    }  
 }
