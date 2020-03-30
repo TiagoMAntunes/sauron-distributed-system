6beeeeb9b74e4f0206cb17cdb8e33a67477c96c5
Gonçalo Almeida <32540757+xaleza@users.noreply.github.com>
2020-03-30 18:06:28 +0100

    fix observationsHashMap

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
@@ -1,9 +1,26 @@
 package pt.tecnico.sauron.silo.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import pt.tecnico.sauron.silo.grpc.Silo.Observation;
+
+
 public class SiloServer {
+
+    private Map<String, ArrayList<Observation>> observationsHashMap = new HashMap<String, ArrayList<Observation>>();
 
     public synchronized boolean clear() {
         return true;
     }
+
+    public synchronized void inputObservation(String identifier , Observation observation) {
+        observationsHashMap.get(identifier).add(observation);
+    }
+
+    public synchronized List<Observation> getObservations(String identifier) {
+        return observationsHashMap.get(identifier);
+    }  
 
 }
