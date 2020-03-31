d6dcd5af260eaa3fc2f6b6d0ef78376dcdb2db82
Tiago Antunes <tiago.melo.antunes@gmail.com>
2020-03-31 14:42:48 +0100

    First implementation of Report command (#22)

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
@@ -1,5 +1,6 @@
 package pt.tecnico.sauron.silo;
 
+import pt.tecnico.sauron.silo.grpc.Silo.Camera;
 import pt.tecnico.sauron.silo.grpc.Silo.ControlClearRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.ControlClearResponse;
 import pt.tecnico.sauron.silo.grpc.Silo.ControlPingRequest;
@@ -8,18 +9,64 @@ import pt.tecnico.sauron.silo.grpc.Silo.ControlInitRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.ControlInitResponse;
 import pt.tecnico.sauron.silo.grpc.Silo.Status;
 import pt.tecnico.sauron.silo.grpc.Silo.Observation;
+import pt.tecnico.sauron.silo.grpc.Silo.ReportRequest;
+import pt.tecnico.sauron.silo.grpc.Silo.ReportResponse;
 import pt.tecnico.sauron.silo.domain.SiloServer;
 import pt.tecnico.sauron.silo.grpc.*;
+import pt.tecnico.sauron.silo.domain.Registry;
 
 import static io.grpc.Status.INVALID_ARGUMENT;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.google.protobuf.Timestamp;
+
 import io.grpc.stub.StreamObserver;
+
+import static com.google.protobuf.util.Timestamps.fromMillis;
+import static java.lang.System.currentTimeMillis;
+
 
 public class SiloServerImpl extends SauronGrpc.SauronImplBase {
 
     private final SiloServer silo = new SiloServer();
+
+    @Override
+    public void report(ReportRequest request, StreamObserver<ReportResponse> responseObserver) {
+        
+        String camName = request.getCameraName();
+        List<Observation> observations = request.getObservationsList();
+        
+        ReportResponse response;
+        if (camName == null) {
+            response = ReportResponse.newBuilder().setResponseStatus(Status.INVALID_ARG).build();
+        } else if (camName.equals("") || !silo.cameraExists(camName)) {
+            response = ReportResponse.newBuilder().setResponseStatus(Status.INVALID_CAM).build();
+        } else if (observations == null) {
+            response = ReportResponse.newBuilder().setResponseStatus(Status.NULL_OBS).build();
+        } else if (observations.isEmpty()) {
+            response = ReportResponse.newBuilder().setResponseStatus(Status.INVALID_OBS).build();
+        } else {
+            //Transform into registries
+            
+            ArrayList<Registry> list = new ArrayList<>();
+            for (Observation o : observations) {
+                Camera cam = o.getCamera();
+                String type = o.getObservated().getType();
+                String id = o.getObservated().getIdentifier();
+                Timestamp time = fromMillis(currentTimeMillis());
+                Registry r = new Registry(cam, type, id, time);
+                list.add(r);
+            }
+            silo.addRegistries(list);
+            
+            response = ReportResponse.newBuilder().setResponseStatus(Status.OK).build();
+        }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
     @Override
     public void controlPing(ControlPingRequest request, StreamObserver<ControlPingResponse> responseObserver) {
--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
@@ -4,23 +4,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import pt.tecnico.sauron.silo.grpc.Silo.Observation;
+
+import pt.tecnico.sauron.silo.grpc.Silo.Camera; //TODO remove import of grpc entity
 
 
 public class SiloServer {
 
-    private Map<String, ArrayList<Observation>> observationsHashMap = new HashMap<String, ArrayList<Observation>>();
+    private Map<String, ArrayList<Registry>> registriesMap = new HashMap<>();
+    private Map<String, Camera> cameras = new HashMap<>();
 
     public synchronized boolean clear() {
         return true;
     }
 
-    public synchronized void inputObservation(String identifier , Observation observation) {
-        observationsHashMap.get(identifier).add(observation);
+    public synchronized void inputRegistry(String identifier , Registry reg) {
+        registriesMap.get(identifier).add(reg);
     }
 
-    public synchronized List<Observation> getObservations(String identifier) {
-        return observationsHashMap.get(identifier);
+    public synchronized List<Registry> getRegistries(String identifier) {
+        return registriesMap.get(identifier);
     }  
+
+    public synchronized boolean cameraExists(String cameraName) {
+        return cameras.containsKey(cameraName);
+    }
+
+    public synchronized void addRegistries(List<Registry> registries) {
+        for (Registry r : registries)
+            if (registriesMap.containsKey(r.getIdentifier()))
+                registriesMap.get(r.getIdentifier()).add(r);
+            else {
+                ArrayList<Registry> list = new ArrayList<>();
+                list.add(r);
+                registriesMap.put(r.getIdentifier(), list);
+            }
+    }
 
 }
