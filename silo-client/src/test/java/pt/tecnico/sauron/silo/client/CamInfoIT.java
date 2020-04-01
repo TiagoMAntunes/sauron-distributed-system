55c5f9950148cbc2c4e955d88a36ed60e4a1b2ac
Gonçalo Almeida <32540757+xaleza@users.noreply.github.com>
2020-04-01 23:18:56 +0100

    T1 tests - Update (#31)
    * Updated T1 tests
    
    * Update CamInfoIT.java
    
    * fix

--- silo-client/src/test/java/pt/tecnico/sauron/silo/client/CamInfoIT.java
+++ silo-client/src/test/java/pt/tecnico/sauron/silo/client/CamInfoIT.java
@@ -1,9 +1,12 @@
 package pt.tecnico.sauron.silo.client;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.grpc.Status.Code.FAILED_PRECONDITION;
+import static io.grpc.Status.Code.INVALID_ARGUMENT;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.google.type.LatLng;
 
+import io.grpc.StatusRuntimeException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import com.google.type.LatLng;
 
 import pt.tecnico.sauron.silo.client.BaseIT;
+import pt.tecnico.sauron.silo.grpc.Silo;
 import pt.tecnico.sauron.silo.grpc.Silo.ControlClearRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.CamInfoRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.CamInfoResponse;
@@ -22,7 +26,7 @@ import pt.tecnico.sauron.silo.grpc.Silo.Camera;
 public class CamInfoIT extends BaseIT {
 
     static final String NAME = "test";
-    static final String INEXISTENT_NAME = "inexistent";
+    static final String INV_NAME = "inexistent";
     static final LatLng COORDS = LatLng.newBuilder().setLatitude(50).setLongitude(50).build();
     static final Camera camera = Camera.newBuilder().setName(NAME).setCoords(COORDS).build();
 
@@ -31,7 +35,7 @@ public class CamInfoIT extends BaseIT {
     @BeforeEach
 	public void setUp() {
         CamJoinRequest request = CamJoinRequest.newBuilder().setCamera(camera).build();
-        CamJoinResponse response = frontend.camJoin(request);
+        frontend.camJoin(request);
     }
 	
 	@AfterEach
@@ -39,22 +43,58 @@ public class CamInfoIT extends BaseIT {
         frontend.controlClear(ControlClearRequest.newBuilder().build());
 	}
 		
-	// tests 
-	
-	@Test
-	public void camInfoTest() {
+	// tests
+
+    @Test
+    public void nonNullResponse() {
+        CamInfoRequest request = Silo.CamInfoRequest.newBuilder().setName(NAME).build();
+        CamInfoResponse response = frontend.camInfo(request);
+        assertNotEquals(null, response, "Response shouldn't be null");
+    }
+
+    @Test
+    public void emptyResponse() {
+        //server has no data
         CamInfoRequest request = CamInfoRequest.newBuilder().setName(NAME).build();
         CamInfoResponse response = frontend.camInfo(request);
-        assertEquals(camera, response.getCamera());
+        assertEquals(
+                FAILED_PRECONDITION,
+                assertThrows(StatusRuntimeException.class, () -> frontend.camInfo(request)).getStatus().getCode()
+        );
     }
-    
-    @Test
-    public void inexistentCameraTest() {
-        CamInfoRequest request = CamInfoRequest.newBuilder().setName(INEXISTENT_NAME).build();
+	
+	@Test
+	public void okResponse() {
+        CamInfoRequest request = CamInfoRequest.newBuilder().setName(NAME).build();
         CamInfoResponse response = frontend.camInfo(request);
 
-        //TODO maybe return Status.INVALID_CAM?
-        assertEquals(Status.INVALID_ARG, response.getResponseStatus());
+        assertEquals(camera, response.getCamera());
     }
+
+    @Test
+    public void noMatch() {
+        //load data first
+        CamInfoRequest request = CamInfoRequest.newBuilder().setName(INV_NAME).build();
+
+        assertEquals(
+                FAILED_PRECONDITION,
+                assertThrows(
+                        StatusRuntimeException.class, () -> frontend.camInfo(request)).getStatus().getCode()
+        );
+    }
+
+    @Test
+    public void emptyName() {
+        CamInfoRequest request = CamInfoRequest.newBuilder().build();
+
+        assertEquals(
+                INVALID_ARGUMENT,
+                assertThrows(
+                        StatusRuntimeException.class, () -> frontend.camInfo(request)).getStatus().getCode()
+        );
+    }
+
+
+
 
 }
