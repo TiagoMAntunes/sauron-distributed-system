35e0ed6921628709243bcc2e0c0409e4b6ac1c1d
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-04-03 13:06:00 +0100

    Added Demo README and files

---
+++ demo/populate/generate.py
@@ -0,0 +1,25 @@
+import random
+
+def run(ops,file):
+    for i,o in enumerate(ops):
+        if i % 5 == 0:
+            file.write('\nzzz,{:d}\n'.format(random.randint(1,1000)))
+        file.write(o + '\n')
+        
+
+
+
+ops = []
+for i in range(1000):
+    for i in range(random.randint(1,10)):
+        ops.append("person,{:d}".format(i))
+
+for i in range(100):
+    for prefix in ["AAAA",'AAAB','AAAC','AAAD','AAAE']:
+        for i in range(random.randint(1,5)):
+            ops.append("car,{:s}{:02d}".format(prefix,i))
+
+for i in range(100):
+    with open('cam{:03d}.txt'.format(i), 'w+') as f:
+        random.shuffle(ops)
+        run(ops[:501], f)
