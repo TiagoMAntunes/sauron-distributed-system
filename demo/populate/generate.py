204a1f78df7a3baab70c43c5446bd86e2f7b5531
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-04-28 18:04:32 +0100

    Changed heavy tests

--- demo/populate/generate.py
+++ demo/populate/generate.py
@@ -3,7 +3,7 @@ import random
 def run(ops,file):
     for i,o in enumerate(ops):
         if i % 5 == 0:
-            file.write('\nzzz,{:d}\n'.format(random.randint(1,1000)))
+            file.write('\nzzz,{:d}\n'.format(random.randint(1000,10000)))
         file.write(o + '\n')
         
 
@@ -19,7 +19,7 @@ for i in range(100):
         for i in range(random.randint(1,5)):
             ops.append("car,{:s}{:02d}".format(prefix,i))
 
-for i in range(100):
+for i in range(30):
     with open('cam{:03d}.txt'.format(i), 'w+') as f:
         random.shuffle(ops)
         run(ops[:501], f)
--- demo/populate/run.sh
+++ demo/populate/run.sh
@@ -3,7 +3,7 @@ FILE_PATH=../../eye/target/appassembler/bin/eye
 
 for file in $(ls cam*.txt);
 do
-    $FILE_PATH localhost 8080 $file 1 1 < $file > $file.out &
+    $FILE_PATH localhost 2181 $file 1 1 < $file > $file.out &
     echo Sending file $file
 done
 
