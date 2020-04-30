30f13a75715a56e896e1a3883612c21f26f778f3
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-04-30 15:20:32 +0100

    Added extra logs

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/SiloServer.java
@@ -159,7 +159,6 @@ public class SiloServer {
         for (Registry r : registries) {
             addRegistry(r); // only add
         }
-        System.out.println("Incrementing multiple registries");
         clock.incUpdate(origin);
     }
 
@@ -170,7 +169,6 @@ public class SiloServer {
      * @param origin the value where it originated to update the clock
      */
     public synchronized void addRegistry(Registry r, int origin) {
-        System.out.println("Incrementing single registry");
         clock.incUpdate(origin);
         addRegistry(r);
     }
@@ -193,13 +191,13 @@ public class SiloServer {
      * @param partialIdentifier the partial identifier
      * @return the all recent registries
      */
-    public synchronized ArrayList<Registry> getAllRecentRegistries(String type, String partialIdentifier) {
+    public synchronized List<Registry> getAllRecentRegistries(String type, String partialIdentifier) {
         Pattern p = Pattern.compile(partialIdentifier.toUpperCase().replace("*",".*"));
         Matcher m;
         ArrayList<Registry> registries = new ArrayList<>();
 
         for (RegistryKey key : registriesMap.keySet()) {
-            if (!key.type.equals(type.toUpperCase())) continue; //different types
+            if (!key.type.equalsIgnoreCase(type)) continue; //different types
             String identifier = key.id;
             m = p.matcher(identifier);
             if (m.matches()) {
