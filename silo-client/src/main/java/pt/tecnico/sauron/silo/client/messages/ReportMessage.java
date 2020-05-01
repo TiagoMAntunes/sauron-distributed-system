cd08ce6020da61daa087b69bc467bae4574f3cfc
istteo <istteogit@gmail.com>
2020-05-01 23:30:48 +0100

    If server crashes before communicating a camjoin the eye wouldn't be able to report, fixed.

--- silo-client/src/main/java/pt/tecnico/sauron/silo/client/messages/ReportMessage.java
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/messages/ReportMessage.java
@@ -1,11 +1,17 @@
 package pt.tecnico.sauron.silo.client.messages;
 
+import pt.tecnico.sauron.silo.client.exceptions.UnavailableException;
 import pt.tecnico.sauron.silo.grpc.SauronGrpc;
 import pt.tecnico.sauron.silo.grpc.Silo.CamJoinRequest;
+import pt.tecnico.sauron.silo.grpc.Silo.CamJoinResponse;
 import pt.tecnico.sauron.silo.grpc.Silo.ReportRequest;
 import pt.tecnico.sauron.silo.grpc.Silo.ReportResponse;
 import pt.tecnico.sauron.silo.grpc.Silo.VectorClock;
 import pt.ulisboa.tecnico.sdis.zk.ZKNamingException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 import com.google.protobuf.Message;
 
 public class ReportMessage implements Request {
@@ -19,7 +25,22 @@ public class ReportMessage implements Request {
 
     public Message call(SauronGrpc.SauronBlockingStub stub, Clock timestamp) throws ZKNamingException { 
         ReportRequest request = ReportRequest.newBuilder().setCameraName(req.getCameraName()).addAllObservations(req.getObservationsList()).setPrev(VectorClock.newBuilder().addAllUpdates(timestamp.getList()).build()).build();
-        ReportResponse response = stub.report(request);
+        ReportResponse response ;
+        try {
+            response = stub.report(request);
+        } catch (RuntimeException e) {
+            //This happens because replica crashed before communicating the join
+            //Create a new camjoin
+            ArrayList<Integer> newLst = new ArrayList<>(Collections.nCopies(timestamp.getList().size(), 0));
+            CamJoinRequest newCamReq = CamJoinRequest.newBuilder().setCamera(camReq.getCamera()).setPrev(VectorClock.newBuilder().addAllUpdates(newLst).build()).build();
+            CamJoinResponse camRes = stub.camJoin(newCamReq);
+            //Add new join timestamp to client
+            timestamp.reset();
+            timestamp.update(camRes.getNew().getUpdatesList());
+            //Make report
+            request = ReportRequest.newBuilder().setCameraName(req.getCameraName()).addAllObservations(req.getObservationsList()).setPrev(VectorClock.newBuilder().addAllUpdates(timestamp.getList()).build()).build();
+            response = stub.report(request);
+        }
         timestamp.update(response.getNew().getUpdatesList());
         return response;
     }
